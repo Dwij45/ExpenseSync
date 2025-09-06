eb373f8411fccf2017d809cbe92a1b2a206b6c44
Dwij <gosaidwij045@gmail.com>
2025-09-06 11:49:22 +0530

    add income schema

--- backend/db.js
+++ backend/db.js
@@ -37,11 +37,39 @@ const ExpenseSchema = new mongoose.Schema({
     required: true
   }
 });
+const IncomeSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: [
+      'Salary', 'Business', 'Investment', 'Gift','Other'
+    ]
+  },
+  comment: {
+    type: String,
+    default: ''
+  },
+  date: {
+    type: Date,
+    required: true
+  }
+});
 
 const ExpenseModel = mongoose.model('Expense', ExpenseSchema);
-const UserModel=mongoose.model('User',UserSchema)
+const UserModel=mongoose.model('User',UserSchema);
+const IncomeModel=mongoose.model('Income',IncomeSchema);
 
 module.exports={
 ExpenseModel,
-UserModel
+UserModel,
+IncomeModel
 }
