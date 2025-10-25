b86aa74535462996bb609968851edf7e7c33877a
Dwij <gosaidwij045@gmail.com>
2025-10-26 00:00:36 +0530

    refoactor: DB to config folder

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,26 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const path = require('path');
+const axios = require('axios')
 
 require('dotenv').config();
 
+const connectDB=require('../backend/config/db.js')
+
 const {userRouter} =require('./router/user')
-const {expenseRouter}=require('./router/dashboard')
+const {expenseRouter}=require('./router/dashboard.js')
+const {downloadRouter}=require('./router/download.js')
+const {uploadRouter}=require('./router/upload.js')
+const {chartRouter} = require('./router/charts.js')
+const {userMiddleware} = require('./middleware/userauth')
 
 const app = express();
+app.use(express.json());
 const mongoURI = process.env.MONGO_URI;
 const cors = require('cors');
 
 app.use(cors({
-//   origin: 'http://127.0.0.1:3000', // or whatever your frontend port is
+//   origin: ['http://127.0.0.1:5500', 'http://localhost:3000'], // or whatever your frontend port is
 //   credentials: true, // needed if using cookies or Authorization header
 //   allowedHeaders: ['Content-Type', 'Authorization']
 }));
@@ -24,18 +32,11 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/user',userRouter);
 app.use('/dashboard',expenseRouter)
+app.use('/download',downloadRouter);
+app.use('/upload',uploadRouter);
+app.use('/charts',chartRouter);
 
-async function main(){
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("Connected to MongoDB successfully");
-        
-        app.listen(3000, () => {
-            console.log("Server listening on port 3000");
-        });
-    } catch (error) {
-        console.error("Failed to connect to MongoDB:", error);
-        // process.exit(1);
-    }
-}
-main();
+connectDB();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
