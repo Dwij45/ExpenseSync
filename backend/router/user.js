d179eee9508fbeed19a0c8fd7ccf9ac79f346462
Dwij <gosaidwij045@gmail.com>
2025-10-27 17:27:33 +0530

    fix(auth): wire user routes to updated controllers

--- backend/router/user.js
+++ backend/router/user.js
@@ -6,79 +6,18 @@ const bcrypt= require('bcrypt')
 
 const {Router} =require('express')
 
-const {UserModel} = require('../db')
-
+const {UserModel} = require('../models/UserModel')
+const  {signup,signin} =require('../controllers/authController')
+// const {UserModel} = require('../db')
 const secretkey =process.env.usersecretkey;
 
-const userRouter=Router();
-userRouter.use(express.json());//! important to parse the body of the request
+const authRouter=Router();
+authRouter.use(express.json());//! important to parse the body of the request
 
+authRouter.post('/signup',signup)
 
-    userRouter.post('/signup',async function (req,res){
-        
-    const email=req.body.email;
-    const password=req.body.password;
-    const name=req.body.name;
-    
-    //input validations
-    const requireBody=z.object({
-        email:z.string().min(3).max(100).email(),
-        password:z.string(),
-        name:z.string().min(3).max(50)
-    })
-    
-    const parsedDataWithSuccess=requireBody.safeParse(req.body)
-    
-    if(!parsedDataWithSuccess.success){
-        res.json({
-            message:"incorrect input",
-            error:parsedDataWithSuccess.error 
-        })
-        const error=parsedDataWithSuccess.error
-        const err = error.issues.map(issue => issue.message);
-        console.log(err);
-    }
-    else{
-        const hasedPassword = await bcrypt.hash(password, 5);
-        
-        await UserModel.create({
-            email: email,
-            password:hasedPassword,
-            name: name
-           
-        });
-        
-        res.json({message: "you are signed up"})
-    } 
-})
-
-    userRouter.post('/signin',async function (req,res){
-        const email=req.body.email;
-        const password=req.body.password;
-
-        //custom function can also be defined 
-        const user =await UserModel.findOne({
-            email:email,
-        })
-        const passwordmatched = bcrypt.compare(password,user.password);
-
-        if (user && passwordmatched) {
-            const token = jwt.sign({
-                id: user._id.toString()
-            }, secretkey);
-    
-            res.json({
-                token
-            })
-            console.log("user._id"+user._id.toString())
-
-        } else {
-            res.status(403).json({
-                message: "Incorrect creds"
-            })
-        }
-    })
+authRouter.post('/signin',signin)
 
 module.exports={
-   userRouter:userRouter
+   authRouter:authRouter
 }
